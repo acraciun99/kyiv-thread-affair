acb7627f89e938b19f5cad0302c60d11d30c9021
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-06-20 22:05:19 +0300

    thread affair tbc

---
+++ components/HeaderSecondary.jsx
@@ -0,0 +1,62 @@
+export default function HeaderSecondary() {
+  return (
+    <section className="header-secondary">
+      <div className="header-logo">
+        <a href="https://pixellab.ro/" title="Thread Affair">
+          <img src="/images/logo.jpg" alt="Thread Affair-Logo"></img>{' '}
+        </a>
+      </div>
+
+      <div className="header-support d-none d-lg-block">
+        <ul>
+          <li>
+            <a href="tel:" title="Call us">
+              Toll Free 1-877-666-1840
+            </a>{' '}
+            |
+          </li>
+
+          <li>
+            <a href="mailto:x@x.ro" title="Email us">
+              office@traidaffairs.com
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header-search d-lg-none">
+        <ul>
+          <li>
+            <a href="https://pixellab.ro" title="Menu">
+              <i className="fa-solid fa-bars"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://pixellab.ro" title="Search">
+              <i className="fa-solid fa-search"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header-controls">
+        <ul>
+          <li>
+            <a href="https://pixellab.ro" title="Cart">
+              <i className="fa-solid fa-cart-shopping"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://pixellab.ro" title="My Account">
+              <img src="/images/user-avatar.png" alt="My Account"></img>
+
+              <i className="fa-solid fa-angle-down"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}

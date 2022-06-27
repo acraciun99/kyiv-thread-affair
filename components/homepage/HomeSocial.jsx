2cc25c4f1a8772602edc455eeefc39e5dcf83498
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-06-27 21:55:55 +0300

    thread affair-main

---
+++ components/homepage/HomeSocial.jsx
@@ -0,0 +1,45 @@
+export default function HomeSocial() {
+  return (
+    <section className="home-social">
+      <header>
+        <h1>
+          <span>We are</span> Social!
+        </h1>
+      </header>
+
+      <section>
+        <ul>
+          <li>
+            <a href="https://x.ro" title="Facebook">
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Instagram">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Linkedin">
+              <i className="fa-brands fa-linkedin-in"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Pinterest">
+              <i className="fa-brands fa-pinterest"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="https://x.ro" title="Twitter">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
+}
